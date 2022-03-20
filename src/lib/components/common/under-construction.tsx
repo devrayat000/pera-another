a5a7cc98702a84852603bf820683f6425d015562
devrayat000 <2010050@me.buet.ac.bd>
2022-03-20 14:29:38 +0600

    implemented under construction page

--- src/lib/components/common/under-construction.tsx
+++ src/lib/components/common/under-construction.tsx
@@ -1,14 +1,26 @@
-import { Typography } from '@mui/material'
 import Image from 'next/image'
+import { Typography, Box } from '@mui/material'
 
 const UnserConstruction = () => {
   return (
     <div>
+      <Box height={t => t.spacing(10)} />
       <Typography variant='h1' component='h1' fontWeight={700}>
-        Under Construction
+        Coming Soon
       </Typography>
-      <Typography variant='h4' component='h4'>
-        Magic is going to happen 😋...
+      <Box height={t => t.spacing(3)} />
+
+      <Typography sx={{ maxWidth: '40ch' }}>
+        This view will allow to manage your daily activities on a calender as
+        event. Also you will be able to synchronize with your google calender.
+        This view will be available on the next update. [Pera v70]
+      </Typography>
+      <Box height={t => t.spacing(5)} />
+
+      <Typography>
+        Warm wishes from,
+        <br /> BooleanWolf
+        <br /> DarkCoder
       </Typography>
       <Image
         src='/images/under_construction.png'
